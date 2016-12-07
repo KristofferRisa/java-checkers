7e15b614018518dec73f881d7aa2140c7b56f140
Kristoffer Risa <kristofferrisa@gmail.com>
2016-12-07 15:32:00 +0100

    flyttet ansvarsområde

--- javaeksamen/src/Views/UserInputView.java
+++ javaeksamen/src/Views/UserInputView.java
@@ -1,9 +1,12 @@
 package Views;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
@@ -17,38 +20,82 @@ import Models.Player;
 
 public class UserInputView extends JPanel implements ActionListener  {
 
-	private JButton button;
-
-	private JCheckBox checkBox;
-
-	public UserInputView(GameConfig config){
-		this.config = config;
-		tf = new JTextField();
-		JLabel l = new JLabel("Skriv inn navn:");
-		button = new JButton("Start");
-		checkBox = new JCheckBox("Server");
-		
-		
-		setSize(400,500);
-		setLayout(new GridLayout(4,0));
-		
-		add(l);
-		add(tf);
-		add(checkBox);
-		add(button);
-	
-		button.addActionListener(this);	
-	}
-	
-	private JTextField tf;
-	
 	public Player p;
+	
 	public GameConfig config;
 	
-	public void actionPerformed(ActionEvent event) {
+	private JTextField nameField;
+
+	private JButton btnServer;
+
+	private JButton btnJoin;
+
+	private JButton btnLocal;
+
+	public UserInputView(GameConfig c){
+		
+		config = c;
+		setBorder(BorderFactory.createTitledBorder("Type in your username and create a new game or join an existing one!"));
+		
+		JLabel nameLabel = new JLabel ("Username: ");
+		
+		nameField = new JTextField(10);
+		
+		btnServer = new JButton("Create New Online Game");
+		btnJoin = new JButton("Join Online Game/Enter IP");
+		btnLocal = new JButton("Test Game Locally");
+		
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.anchor = GridBagConstraints.LINE_START;
+		
+		//Label and TextField  -------------------
+		
+		gc.weightx = 0.5;
+		gc.weighty = 0.5;
+		
+		gc.gridx = 0;
+		gc.gridy = 0;
+		add(nameLabel, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 0;
+		add(nameField, gc);
+		
+		//First button - Create Game  -------------------
+		gc.gridx = 1;
+		gc.gridy = 2;
+		add(btnServer, gc);
+		
+		//Second button - Join Game  -------------------
+		gc.gridx = 1;
+		gc.gridy = 3;
+		add(btnJoin, gc);
+		
+		gc.weighty = 10;
+		gc.weightx = 10;
+		
+		//Third button - Test Game  -------------------
+		gc.gridx = 1;
+		gc.gridy = 4;
+		add(btnLocal, gc);
+		btnServer.addActionListener(this);
+		btnJoin.addActionListener(this);
+		
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) {
 		if(p != null && config != null){
-			p.name = tf.getText();
-			config.isServer = checkBox.isSelected();
+			
+			p.name = nameField.getText();
+			
+		if(e.getSource() == btnServer)
+			config.isServer = true;
+		}
+		if(e.getSource() == btnJoin){
+			config.isServer = false;
 		}
 		setVisible(false);
     }
--- javaeksamen/src/Views/WindowManager.java
+++ javaeksamen/src/Views/WindowManager.java
@@ -4,39 +4,49 @@ import javax.swing.JFrame;
 
 import Models.Game;
 import Models.GameConfig;
+import Models.GameData;
+import Models.Klient;
 import Models.Player;
+import Models.Server;
 
-public class WindowManager extends JFrame {
+public class WindowManager extends Thread {
 	
 	public Player player;
 	public Game game;
 	public DebugView debug;
 	private GameView gameView;
-	private UserInputView userInputView;
+	private WindowContainer windowContainer;
+	private GameConfig config;
+	private Server server;
+	private Klient klient;
+	private GameData gameData;
 	
-	public WindowManager(Game game, Player player,GameConfig config){
-		this.game = game;
-		this.player = player;
-		userInputView = new UserInputView(config);
+	public WindowManager(Game g, Player p, GameConfig c){
+		game = g;
+		player = p;
+		config = c;
 		gameView = new GameView();
+		windowContainer = new WindowContainer(c);
 		
-		configureFrame();
 		
-		add(userInputView);
 		debug = new DebugView();
 	}
-
-	private void configureFrame() {
-		setTitle("Checkers 1.0!! (java eksamen)");
-		setVisible(true);
-		setSize(400,500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
-	}
 	
-	public void showUserInput(){
-		userInputView.p = this.player;
-		userInputView.setVisible(true);
+	public void startUserview(){
+		
+		windowContainer.userInputView.p = this.player;
+		windowContainer.setVisible(true);
+		
+		while(userInputIsActive() == true){
+			try{
+			debug.log("Venter på user input");
+				sleep(2000);
+			}
+			catch(InterruptedException e){
+				debug.log(e.getMessage());
+			}			
+		}
+		
 	}
 	
 	public void showGameView(){
@@ -44,16 +54,55 @@ public class WindowManager extends JFrame {
 	}
 	
 	public void closeUserInput(){
-		userInputView.setVisible(false);
+		windowContainer.userInputView.setVisible(false);
 		notifyAll();
 	}
 	
 	public boolean userInputIsActive(){
-		return (userInputView.isVisible() == true) ? true : false;
+		return (windowContainer.userInputView.isVisible() == true) ? true : false;
 	}
 
 	public boolean gameIsActive() {
 		return (game.isActive == true) ? true : false;
+	}
+
+
+
+	public void startGameView() {
+		if(config.isServer == true){
+			//Start server
+			server = new Server(gameData, debug);
+			server.start();
+			while(server.isConnected == false){
+				try {
+					debug.log("venter på spiller");
+					sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} 
+		else 
+		{
+			//Klient
+			debug.log("Starter ny klient");
+			klient = new Klient(debug);
+			klient.start();
+			debug.log("Forsøker å kople til server");
+			while(klient.isConnected == false){
+				try {
+					debug.log("Venter på Server");
+					sleep(1000);
+					
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			
+		}
 	}
 
 	
