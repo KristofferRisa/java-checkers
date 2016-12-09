1f189a16ea09c112087141123aee7779dd597f9c
Kristoffer Risa <kristofferrisa@gmail.com>
2016-12-09 06:31:44 +0100

    endringer

--- javaeksamen/src/Models/Server.java
+++ javaeksamen/src/Models/Server.java
@@ -8,55 +8,61 @@ import java.net.Socket;
 import Views.DebugView;
 
 public class Server extends Thread {
-	
-	public Server(Game g, DebugView d){
+
+	public Server(Game g,DebugView d){
 		game = g;
 		debug = d;
 		isConnected = false;
 	}
-	
-	private ServerSocket server;
-	private Socket socket;
-	private Game game;
+    
+	public void stopServer()
+    {
+        running = false;
+        this.interrupt();
+    }
 	
 	public boolean isConnected;
 	
 	public void run(){
-		
-	    try {
-	        server = new ServerSocket(1337);
-	        socket = new Socket();
-	        debug.log("Waiting for clients to connect...");
-	        while( (socket = server.accept()) != null){
-	        //while( (socket = server.accept()) != null){
-	        	debug.log("Ny klient tilkoplet!");
-	        	isConnected = true;
-	        	InputStream inputStream = socket.getInputStream();
-	        	ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-				
-	        	output.writeObject(game);
-				output.flush();
-				
-				break;
-//				byte[] buffer = new byte[1024];
-//			    int read;
-//			    while((read = inputStream.read(buffer)) != -1) {
-//			        String out = new String(buffer, 0, read);
-//			        debug.log(out);
-//			        System.out.flush();
-//			    };
-				
-	        }
-	    } catch (IOException e) {
-	        debug.log("Unable to process client request");
-	        e.printStackTrace();
-	    }		
+		running = true;
+        while( running )
+        {
+        	//http://www.javaworld.com/article/2853780/core-java/socket-programming-for-scalable-systems.html?page=2
+
+		    try {
+		        server = new ServerSocket(1337);
+		        debug.log("Waiting for clients to connect...");
+		        
+		        socket = server.accept();
+		        debug.log("klient koplet til");
+		        
+		    } catch (IOException e) {
+		        debug.log("Unable to process client request");
+		        e.printStackTrace();
+		    }		
+        }
 	    debug.log("ferdig med klient!");
 	}
 	
 	public void sendGame(){
 		
+		try {
+			ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+			output.writeObject(game);
+	    	output.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
+    
+	private ServerSocket server;
+	private Socket socket;
+	private Game game;
+	private boolean running;
+
 	
 	private DebugView debug;
 	
