083e7c04b96da4418d70c120195a58304d30e418
pnitram <martin.pedersen@me.com>
2016-12-15 00:17:09 +0100

    La til menypunkt for å skr av mus+ actionlilstner. Fikk ikke til toggle

---
+++ javaeksamen/src/graphics/BaseMouseListener.java
@@ -0,0 +1,20 @@
+package graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public abstract class BaseMouseListener implements ActionListener{
+
+    private static boolean active = true;
+    public static void setActive(boolean active){
+        BaseMouseListener.active = active;
+    }
+
+    protected abstract void doPerformAction(ActionEvent e);
+
+    @Override
+    public final void actionPerformed(ActionEvent e){
+        if(active){
+            doPerformAction(e);
+        }
+    }
+}
