9c8cdc6d172579c6e090890dc174893588cdcdfd
Kristoffer Risa <kristofferrisa@gmail.com>
2016-12-15 13:51:15 +0100

    oppdatert server

--- javaeksamen/src/network/Server.java
+++ javaeksamen/src/network/Server.java
@@ -82,7 +82,7 @@ public class Server extends Thread {
 				GameDataDTO datare = client1.recive();
 				System.out.println("server: mottok data fra klient 1");
 				if(datare.move == null){
-					System.out.println("server: mangler move data klient");
+					System.out.println("server: mangler move data klient 1");
 				}
 				else {
 					System.out.println("server: brikke flytte fra row " + datare.move.oldPostionCol + " col " + datare.move.oldPostionRow);	
@@ -104,16 +104,40 @@ public class Server extends Thread {
 				datare.msg = "FLYTT";
 				//dataTransferObject = checkersEngine.validate(dataTransferObject);
 				client1.send(datare);
+				dataTransferObject.clientIdTurn = 2;
 			
 			}
 
 			while (dataTransferObject.clientIdTurn == 2) {
 				
-				client2.recive();
-				System.out.println("server: mottok data fra klient 1");
-				System.out.println("server: brikke flytte fra row " + dataTransferObject.move.oldcx + " col " + dataTransferObject.move.oldcy );
-				dataTransferObject = checkersEngine.validate(dataTransferObject);
 				client2.send(dataTransferObject);
+				
+				GameDataDTO datare = client1.recive();
+				System.out.println("server: mottok data fra klient 2");
+				
+				if(datare.move == null){
+					System.out.println("server: mangler move data klient 2");
+				}
+				else {
+					System.out.println("server: brikke flytte fra row " + datare.move.oldPostionCol + " col " + datare.move.oldPostionRow);	
+				}
+				System.out.println("server: ny melding fra klient "+ dataTransferObject.msg);
+				
+				
+				// Do not move checker onto an occupied square.
+				for (PostionValidator _pv : datare.pieces){
+					if (_pv != datare.postionValidator 
+							&& _pv.cx == datare.postionValidator.cx
+								&& _pv.cy == datare.postionValidator.cy) {
+						//Dette skal sendes tilbaek
+						datare.postionValidator.cx = datare.move.oldcx;
+						datare.postionValidator.cy = datare.move.oldcy;
+					}
+				}
+				
+				datare.msg = "FLYTT";
+				//dataTransferObject = checkersEngine.validate(dataTransferObject);
+				client2.send(datare);
 				
 			}
 
